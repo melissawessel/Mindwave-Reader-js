fd827b1bc79796c2263fbfa423a4dd619c577081
Melissa <melissa@Melissas-MacBook-Pro.local>
2017-05-30 01:17:08 -0400

    simpler method

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
-var neurosky = require('node-neurosky');
+// var neurosky = require('node-neurosky');
+//
+// var client = neurosky.createClient({
+// 	appName: 'Anything2',
+// 	appKey: 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+// });
+//
+// client.on('data',function(data){
+//
+// 	console.log('hi');
+//
+// });
+//
+// client.connect(function() {
+// 	console.log('worked');
+//
+// }, function() {
+// 	console.log('oh no');
+// }
+// );
 
-var client = neurosky.createClient({
-	appName: 'NodeNeuroSky',
-	appKey: '0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
+var Mindwave = require('mindwave');
+var mw = new Mindwave();
+
+mw.on('eeg', function(eeg){
+    console.log('eeg', eeg);
 });
 
-client.on('data',function(data){
-
-	console.log('hi');
-
+mw.on('signal', function(signal){
+    console.log('signal', signal);
 });
 
-client.connect();
+mw.on('attention', function(attention){
+    console.log('attention', attention);
+});
+
+mw.on('meditation', function(meditation){
+    console.log('meditation', meditation);
+});
+
+mw.on('blink', function(blink){
+    console.log('blink', blink);
+});
+
+// These are the raw EEG data
+// They come in at about 512Hz
+// mw.on('wave', function(wave){
+// 	console.log('wave', wave);
+// });
+
+mw.connect('/dev/tty.MindWaveMobile-SerialPo');
--- node_modules/serialport/lib/serialport.js
+++ node_modules/serialport/lib/serialport.js
@@ -2,10 +2,6 @@
 
 // Copyright 2011 Chris Williams <chris@iterativedesigns.com>
 
-// Require serialport binding from pre-compiled binaries using
-// node-pre-gyp, if something fails or package not available fallback
-// to regular build from source.
-
 // 3rd Party Dependencies
 var debug = require('debug')('serialport');
 
@@ -17,15 +13,9 @@ var SerialPortBinding = require('./bindings');
 var parsers = require('./parsers');
 
 // Built-ins Dependencies
-var EventEmitter = require('events').EventEmitter;
 var fs = require('fs');
 var stream = require('stream');
 var util = require('util');
-
-// Setup the factory
-var factory = new EventEmitter();
-factory.parsers = parsers;
-factory.list = SerialPortBinding.list;
 
 //  VALIDATION ARRAYS
 var DATABITS = [5, 6, 7, 8];
@@ -40,6 +30,7 @@ var kMinPoolSpace = 128;
 
 var defaultSettings = {
   baudRate: 9600,
+  autoOpen: true,
   parity: 'none',
   xon: false,
   xoff: false,
@@ -49,6 +40,7 @@ var defaultSettings = {
   dataBits: 8,
   stopBits: 1,
   bufferSize: 64 * 1024,
+  lock: true,
   parser: parsers.raw,
   platformOptions: SerialPortBinding.platformOptions
 };
@@ -67,8 +59,7 @@ var LOWERCASE_OPTIONS = [
   'dataBits',
   'stopBits',
   'bufferSize',
-  'platformOptions',
-  'flowControl'
+  'platformOptions'
 ];
 
 function correctOptions(options) {
@@ -83,80 +74,53 @@ function correctOptions(options) {
   return options;
 }
 
-function SerialPort(path, options, openImmediately, callback) {
-  var args = Array.prototype.slice.call(arguments);
-  callback = args.pop();
-  if (typeof callback !== 'function') {
-    callback = null;
+function SerialPort(path, options, callback) {
+  if (typeof callback === 'boolean') {
+    throw new TypeError('`openImmediately` is now called `autoOpen` and is a property of options');
   }
 
-  options = (typeof options !== 'function') && options || {};
-
-  if (openImmediately === undefined || openImmediately === null) {
-    openImmediately = true;
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
   }
+
+  options = options || {};
 
   stream.Stream.call(this);
-  callback = callback || function (err) {
-    if (err) {
-      if (this._events.error) {
-        this.emit('error', err);
-      } else {
-        factory.emit('error', err);
-      }
-    }
-  }.bind(this);
 
   if (!path) {
-    return callback(new Error('Invalid port specified: ' + path));
+    throw new TypeError('No path specified');
   }
+
   this.path = path;
 
   var correctedOptions = correctOptions(options);
   var settings = assign({}, defaultSettings, correctedOptions);
 
+  if (typeof settings.baudRate !== 'number') {
+    throw new TypeError('Invalid "baudRate" must be a number got: ' + settings.baudRate);
+  }
+
   if (DATABITS.indexOf(settings.dataBits) === -1) {
-    return callback(new Error('Invalid "databits": ' + settings.dataBits));
+    throw new TypeError('Invalid "databits": ' + settings.dataBits);
   }
 
   if (STOPBITS.indexOf(settings.stopBits) === -1) {
-    return callback(new Error('Invalid "stopbits": ' + settings.stopbits));
+    throw new TypeError('Invalid "stopbits": ' + settings.stopbits);
   }
 
   if (PARITY.indexOf(settings.parity) === -1) {
-    return callback(new Error('Invalid "parity": ' + settings.parity));
+    throw new TypeError('Invalid "parity": ' + settings.parity);
   }
 
-  var fc = settings.flowControl;
-  if (fc === true) {
-    // Why!?
-    settings.rtscts = true;
-  } else if (Array.isArray(fc)) {
-    for (var i = fc.length - 1; i >= 0; i--) {
-      var fcSetting = fc[i].toLowerCase();
-      if (FLOWCONTROLS.indexOf(fcSetting) > -1) {
-        settings[fcSetting] = true;
-      } else {
-        return callback(new Error('Invalid flowControl option: ' + fcSetting));
-      }
+  FLOWCONTROLS.forEach(function(control) {
+    if (typeof settings[control] !== 'boolean') {
+      throw new TypeError('Invalid "' + control + '" is not boolean');
     }
-  }
+  });
 
-  // TODO remove this option
-  settings.dataCallback = options.dataCallback || function (data) {
-    settings.parser(this, data);
-  }.bind(this);
-
-  // TODO remove this option
-  settings.disconnectedCallback = options.disconnectedCallback || function (err) {
-    if (this.closing) {
-      return;
-    }
-    if (!err) {
-      err = new Error('Disconnected');
-    }
-    this.emit('disconnect', err);
-  }.bind(this);
+  settings.disconnectedCallback = this._disconnected.bind(this);
+  settings.dataCallback = settings.parser.bind(this, this);
 
   this.fd = null;
   this.paused = true;
@@ -171,25 +135,23 @@ function SerialPort(path, options, openImmediately, callback) {
 
   this.options = settings;
 
-  if (openImmediately) {
-    process.nextTick(function () {
-      this.open(callback);
-    }.bind(this));
+  if (this.options.autoOpen) {
+    // is nextTick necessary?
+    process.nextTick(this.open.bind(this, callback));
   }
 }
 
-factory.SerialPort = SerialPort;
 util.inherits(SerialPort, stream.Stream);
 
 SerialPort.prototype._error = function(error, callback) {
   if (callback) {
-    callback(error);
+    callback.call(this, error);
   } else {
     this.emit('error', error);
   }
 };
 
-SerialPort.prototype.open = function (callback) {
+SerialPort.prototype.open = function(callback) {
   if (this.isOpen()) {
     return this._error(new Error('Port is already open'), callback);
   }
@@ -203,36 +165,32 @@ SerialPort.prototype.open = function (callback) {
   this.reading = false;
   this.opening = true;
 
-  var self = this;
-  SerialPortBinding.open(this.path, this.options, function (err, fd) {
+  SerialPortBinding.open(this.path, this.options, function(err, fd) {
+    this.opening = false;
     if (err) {
       debug('SerialPortBinding.open had an error', err);
-      return self._error(err, callback);
+      return this._error(err, callback);
     }
-    self.fd = fd;
-    self.paused = false;
-    self.opening = false;
+    this.fd = fd;
+    this.paused = false;
 
     if (process.platform !== 'win32') {
-      self.serialPoller = new SerialPortBinding.SerialportPoller(self.fd, function (err) {
+      this.serialPoller = new SerialPortBinding.SerialportPoller(this.fd, function(err) {
         if (!err) {
-          self._read();
+          this._read();
         } else {
-          self.disconnected(err);
+          this._disconnected(err);
         }
-      });
-      self.serialPoller.start();
+      }.bind(this));
+      this.serialPoller.start();
     }
 
-    self.emit('open');
-    if (callback) { callback() }
-  });
+    this.emit('open');
+    if (callback) { callback.call(this, null) }
+  }.bind(this));
 };
 
-// underlying code is written to update all options, but for now
-// only baud is respected as I don't want to duplicate all the option
-// verification code above
-SerialPort.prototype.update = function (options, callback) {
+SerialPort.prototype.update = function(options, callback) {
   if (!this.isOpen()) {
     debug('update attempted, but port is not open');
     return this._error(new Error('Port is not open'), callback);
@@ -242,12 +200,11 @@ SerialPort.prototype.update = function (options, callback) {
   var settings = assign({}, defaultSettings, correctedOptions);
   this.options.baudRate = settings.baudRate;
 
-  SerialPortBinding.update(this.fd, this.options, function (err) {
+  SerialPortBinding.update(this.fd, this.options, function(err) {
     if (err) {
       return this._error(err, callback);
     }
-    this.emit('open');
-    if (callback) { callback() }
+    if (callback) { callback.call(this, null) }
   }.bind(this));
 };
 
@@ -255,7 +212,7 @@ SerialPort.prototype.isOpen = function() {
   return this.fd !== null && !this.closing;
 };
 
-SerialPort.prototype.write = function (buffer, callback) {
+SerialPort.prototype.write = function(buffer, callback) {
   if (!this.isOpen()) {
     debug('write attempted, but port is not open');
     return this._error(new Error('Port is not open'), callback);
@@ -264,111 +221,109 @@ SerialPort.prototype.write = function (buffer, callback) {
   if (!Buffer.isBuffer(buffer)) {
     buffer = new Buffer(buffer);
   }
-  debug('write data: ' + JSON.stringify(buffer));
 
-  var self = this;
-  SerialPortBinding.write(this.fd, buffer, function (err, results) {
-    if (callback) {
-      callback(err, results);
-    } else {
-      if (err) {
-        self.emit('error', err);
-      }
+  debug('write ' + buffer.length + ' bytes of data');
+  SerialPortBinding.write(this.fd, buffer, function(err) {
+    if (err) {
+      debug('SerialPortBinding.write had an error', err);
+      return this._error(err, callback);
     }
-  });
+    if (callback) { callback.call(this, null) }
+  }.bind(this));
 };
 
 if (process.platform !== 'win32') {
-  SerialPort.prototype._read = function () {
-    var self = this;
-    if (!self.readable || self.paused || self.reading || this.closing) {
+  SerialPort.prototype._read = function() {
+    if (!this.readable || this.paused || this.reading || this.closing) {
       return;
     }
 
-    self.reading = true;
+    this.reading = true;
 
-    if (!self.pool || self.pool.length - self.pool.used < kMinPoolSpace) {
+    if (!this.pool || this.pool.length - this.pool.used < kMinPoolSpace) {
       // discard the old pool. Can't add to the free list because
       // users might have references to slices on it.
-      self.pool = new Buffer(kPoolSize);
-      self.pool.used = 0;
+      this.pool = new Buffer(kPoolSize);
+      this.pool.used = 0;
     }
 
     // Grab another reference to the pool in the case that while we're in the
     // thread pool another read() finishes up the pool, and allocates a new
     // one.
-    var toRead = Math.min(self.pool.length - self.pool.used, ~~self.bufferSize);
-    var start = self.pool.used;
+    var toRead = Math.min(this.pool.length - this.pool.used, ~~this.bufferSize);
+    var start = this.pool.used;
 
-    function afterRead(err, bytesRead, readPool, bytesRequested) {
-      self.reading = false;
+    var _afterRead = function(err, bytesRead, readPool, bytesRequested) {
+      this.reading = false;
       if (err) {
         if (err.code && err.code === 'EAGAIN') {
-          if (!self.closing && self.isOpen()) {
-            self.serialPoller.start();
+          if (this.isOpen()) {
+            this.serialPoller.start();
           }
         // handle edge case were mac/unix doesn't clearly know the error.
         } else if (err.code && (err.code === 'EBADF' || err.code === 'ENXIO' || (err.errno === -1 || err.code === 'UNKNOWN'))) {
-          self.disconnected(err);
+          this._disconnected(err);
         } else {
-          self.fd = null;
-          self.readable = false;
-          self.emit('error', err);
+          this.fd = null;
+          this.readable = false;
+          this.emit('error', err);
+        }
+        return;
+      }
+
+      // Since we will often not read the number of bytes requested,
+      // let's mark the ones we didn't need as available again.
+      this.pool.used -= bytesRequested - bytesRead;
+
+      if (bytesRead === 0) {
+        if (this.isOpen()) {
+          this.serialPoller.start();
         }
       } else {
-        // Since we will often not read the number of bytes requested,
-        // let's mark the ones we didn't need as available again.
-        self.pool.used -= bytesRequested - bytesRead;
+        var b = this.pool.slice(start, start + bytesRead);
 
-        if (bytesRead === 0) {
-          if (self.isOpen()) {
-            self.serialPoller.start();
+        // do not emit events if the stream is paused
+        if (this.paused) {
+          if (!this.buffer) {
+            this.buffer = new Buffer(0);
           }
-        } else {
-          var b = self.pool.slice(start, start + bytesRead);
-
-          // do not emit events if the stream is paused
-          if (self.paused) {
-            self.buffer = Buffer.concat([self.buffer, b]);
-            return;
-          }
-          self._emitData(b);
-
-          // do not emit events anymore after we declared the stream unreadable
-          if (!self.readable) {
-            return;
-          }
-          self._read();
+          this.buffer = Buffer.concat([this.buffer, b]);
+          return;
         }
+        this._emitData(b);
+
+        // do not emit events anymore after we declared the stream unreadable
+        if (!this.readable) {
+          return;
+        }
+        this._read();
       }
-    }
+    }.bind(this);
 
-    fs.read(self.fd, self.pool, self.pool.used, toRead, null, function (err, bytesRead) {
-      var readPool = self.pool;
+    fs.read(this.fd, this.pool, this.pool.used, toRead, null, function(err, bytesRead) {
+      var readPool = this.pool;
       var bytesRequested = toRead;
-      afterRead(err, bytesRead, readPool, bytesRequested);
-    });
+      _afterRead(err, bytesRead, readPool, bytesRequested);
+    }.bind(this));
 
-    self.pool.used += toRead;
+    this.pool.used += toRead;
   };
 
-  SerialPort.prototype._emitData = function (data) {
+  SerialPort.prototype._emitData = function(data) {
     this.options.dataCallback(data);
   };
 
-  SerialPort.prototype.pause = function () {
-    var self = this;
-    self.paused = true;
+  SerialPort.prototype.pause = function() {
+    this.paused = true;
   };
 
-  SerialPort.prototype.resume = function () {
-    var self = this;
-    self.paused = false;
+  SerialPort.prototype.resume = function() {
+    this.paused = false;
 
-    if (self.buffer) {
-      var buffer = self.buffer;
-      self.buffer = null;
-      self._emitData(buffer);
+    if (this.buffer) {
+      var buffer = this.buffer;
+      this.buffer = null;
+      this._emitData(buffer);
     }
 
     // No longer open?
@@ -376,57 +331,41 @@ if (process.platform !== 'win32') {
       return;
     }
 
-    self._read();
+    this._read();
   };
 } // if !'win32'
 
-SerialPort.prototype.disconnected = function (err) {
-  var self = this;
-  var fd = self.fd;
-
-  // send notification of disconnect
-  if (self.options.disconnectedCallback) {
-    self.options.disconnectedCallback(err);
-  } else {
-    self.emit('disconnect', err);
-  }
-  self.paused = true;
-  self.closing = true;
-
-  self.emit('close');
-
-  // clean up all other items
-  fd = self.fd;
-
-  try {
-    SerialPortBinding.close(fd, function (err) {
-      if (err) {
-        debug('Disconnect completed with error: ' + JSON.stringify(err));
-      } else {
-        debug('Disconnect completed.');
-      }
-    });
-  } catch (e) {
-    debug('Disconnect completed with an exception: ' + JSON.stringify(e));
+SerialPort.prototype._disconnected = function(err) {
+  this.paused = true;
+  this.emit('disconnect', err);
+  if (this.closing) {
+    return;
   }
 
-  // TODO THIS IS CRAZY TOWN
-  self.removeAllListeners();
+  if (this.fd === null) {
+    return;
+  }
 
-  self.closing = false;
-  self.fd = null;
-
+  this.closing = true;
   if (process.platform !== 'win32') {
-    self.readable = false;
-    self.serialPoller.close();
+    this.readable = false;
+    this.serialPoller.close();
   }
+
+  SerialPortBinding.close(this.fd, function(err) {
+    this.closing = false;
+    if (err) {
+      debug('Disconnect close completed with error: ', err);
+    }
+    this.fd = null;
+    this.emit('close');
+  }.bind(this));
 };
 
-SerialPort.prototype.close = function (callback) {
-  var self = this;
-  var fd = self.fd;
+SerialPort.prototype.close = function(callback) {
+  this.paused = true;
 
-  if (self.closing) {
+  if (this.closing) {
     debug('close attempted, but port is already closing');
     return this._error(new Error('Port is not open'), callback);
   }
@@ -436,53 +375,42 @@ SerialPort.prototype.close = function (callback) {
     return this._error(new Error('Port is not open'), callback);
   }
 
-  self.closing = true;
+  this.closing = true;
 
   // Stop polling before closing the port.
   if (process.platform !== 'win32') {
-    self.readable = false;
-    self.serialPoller.close();
+    this.readable = false;
+    this.serialPoller.close();
   }
-
-  try {
-    SerialPortBinding.close(fd, function (err) {
-      self.closing = false;
-      if (err) {
-        debug('SerialPortBinding.close had an error', err);
-        return self._error(err, callback);
-      }
-
-      self.fd = null;
-      self.emit('close');
-      if (callback) { callback() }
-      self.removeAllListeners();
-    });
-  } catch (err) {
+  SerialPortBinding.close(this.fd, function(err) {
     this.closing = false;
-    debug('SerialPortBinding.close had an throwing error', err);
-    return this._error(err, callback);
-  }
+    if (err) {
+      debug('SerialPortBinding.close had an error', err);
+      return this._error(err, callback);
+    }
+
+    this.fd = null;
+    this.emit('close');
+    if (callback) { callback.call(this, null) }
+  }.bind(this));
 };
 
-SerialPort.prototype.flush = function (callback) {
-  var self = this;
-  var fd = self.fd;
-
+SerialPort.prototype.flush = function(callback) {
   if (!this.isOpen()) {
     debug('flush attempted, but port is not open');
     return this._error(new Error('Port is not open'), callback);
   }
 
-  SerialPortBinding.flush(fd, function (err, result) {
-    if (callback) {
-      callback(err, result);
-    } else if (err) {
-      self.emit('error', err);
+  SerialPortBinding.flush(this.fd, function(err, result) {
+    if (err) {
+      debug('SerialPortBinding.flush had an error', err);
+      return this._error(err, callback);
     }
-  });
+    if (callback) { callback.call(this, null, result) }
+  }.bind(this));
 };
 
-SerialPort.prototype.set = function (options, callback) {
+SerialPort.prototype.set = function(options, callback) {
   if (!this.isOpen()) {
     debug('set attempted, but port is not open');
     return this._error(new Error('Port is not open'), callback);
@@ -504,31 +432,43 @@ SerialPort.prototype.set = function (options, callback) {
     }
   }
 
-  SerialPortBinding.set(this.fd, settings, function (err, result) {
+  SerialPortBinding.set(this.fd, settings, function(err) {
     if (err) {
       debug('SerialPortBinding.set had an error', err);
       return this._error(err, callback);
     }
-    if (callback) { callback(null, result) }
+    if (callback) { callback.call(this, null) }
   }.bind(this));
 };
 
-SerialPort.prototype.drain = function (callback) {
-  var self = this;
-  var fd = this.fd;
-
+SerialPort.prototype.drain = function(callback) {
   if (!this.isOpen()) {
     debug('drain attempted, but port is not open');
     return this._error(new Error('Port is not open'), callback);
   }
 
-  SerialPortBinding.drain(fd, function (err, result) {
-    if (callback) {
-      callback(err, result);
-    } else if (err) {
-      self.emit('error', err);
+  SerialPortBinding.drain(this.fd, function(err) {
+    if (err) {
+      debug('SerialPortBinding.drain had an error', err);
+      return this._error(err, callback);
     }
-  });
+    if (callback) { callback.call(this, null) }
+  }.bind(this));
 };
 
-module.exports = factory;
+SerialPort.parsers = parsers;
+SerialPort.list = SerialPortBinding.list;
+
+// Write a depreciation warning once
+Object.defineProperty(SerialPort, 'SerialPort', {
+  get: function() {
+    console.warn('DEPRECATION: Please use `require(\'serialport\')` instead of `require(\'serialport\').SerialPort`');
+    Object.defineProperty(SerialPort, 'SerialPort', {
+      value: SerialPort
+    });
+    return SerialPort;
+  },
+  configurable: true
+});
+
+module.exports = SerialPort;
