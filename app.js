d65f217ef387a6c91ba0741a0203bf1275f17c81
Melissa <melissa@Melissas-MBP.lan>
2017-06-15 17:48:07 -0400

    Add csv option and Philips Hue control

--- app.js
+++ app.js
@@ -1,46 +1,75 @@
-// var neurosky = require('node-neurosky');
-//
-// var client = neurosky.createClient({
-// 	appName: 'Anything2',
-// 	appKey: 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
-// });
-//
-// client.on('data',function(data){
-//
-// 	console.log('hi');
-//
-// });
-//
-// client.connect(function() {
-// 	console.log('worked');
-//
-// }, function() {
-// 	console.log('oh no');
-// }
-// );
 
 var Mindwave = require('mindwave');
 var mw = new Mindwave();
 
-mw.on('eeg', function(eeg){
-    console.log('eeg', eeg);
-});
+var hue = require("node-hue-api"),
+    HueApi = hue.HueApi,
+    lightState = hue.lightState;
 
-mw.on('signal', function(signal){
-    console.log('signal', signal);
-});
+var host = "192.168.0.2",
+    username = "5s27H-tZVGzQ8IwisyTCo6dOq9cEgoPhuhdpkqMC",
+    api = new HueApi(host, username),
+    state;
+
+
+function printToCsv(eegData) {
+  var fs = require('fs');
+  var json2csv = require('json2csv');
+  var newLine= "\r\n";
+
+  var toCsv = {
+      data: eegData,
+      hasCSVColumnTitle: false
+  };
+
+  var csv = json2csv(toCsv) + newLine;
+  console.log('csv', csv);
+
+  fs.appendFile('unsteady.csv', csv, function (err) {
+    if (err) throw err;
+    //console.log('Appended: ', csv);
+  });
+}
+
+function attnColorChange(attn) {
+  rVal = (attn * 255) / 100
+  bVal = ((100 - attn) * 255) / 100
+  state = lightState.create().on().brightness(attn)
+                    .rgb(rVal,0,bVal).transitionSlow();
+  api.setLightState(2, state)
+  api.setLightState(3, state)
+}
+
+function medColorChange(med) {
+  bVal = (med * 255) / 100
+  rVal = ((100 - med) * 255) / 100
+  state = lightState.create().on().brightness(bVal).saturation(bVal)
+                    .rgb(0,0,bVal).transitionSlow();
+  api.setLightState(4, state)
+  // api.setLightState(3, state)
+}
+
+// mw.on('eeg', function(eeg){
+//     console.log('eeg', eeg);
+// });
+
+// mw.on('signal', function(signal){
+//     console.log('poor signal', signal);
+// });
 
 mw.on('attention', function(attention){
     console.log('attention', attention);
+    attnColorChange(attention)
 });
 
 mw.on('meditation', function(meditation){
     console.log('meditation', meditation);
+    medColorChange(meditation);
 });
 
-mw.on('blink', function(blink){
-    console.log('blink', blink);
-});
+// mw.on('blink', function(blink){
+//     console.log('blink', blink);
+// });
 
 // These are the raw EEG data
 // They come in at about 512Hz
